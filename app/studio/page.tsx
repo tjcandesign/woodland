fc1bd715cb72c92ebe3262730bb5dc7d30276ff4
Tj Cichecki <tj@wrkhrs.co>
2026-05-01 12:03:08 -0400

    Auto-commit changes

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -2,10 +2,6 @@ import StudioClient from './StudioClient';
 
 export const dynamic = 'force-static';
 
-export function generateStaticParams() {
-  return [{ tool: [] as string[] }];
-}
-
 export default function StudioPage() {
   return <StudioClient />;
 }
