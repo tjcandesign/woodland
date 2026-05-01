71b4d6da02271f04dce2e8e44dffa79b70bc10da
Tj Cichecki <tj@wrkhrs.co>
2026-05-01 09:37:37 -0400

    Auto-commit changes

--- app/studio/[[...tool]]/StudioClient.tsx
+++ app/studio/[[...tool]]/StudioClient.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { NextStudio } from 'next-sanity/studio';
-import config from '../../../sanity.config';
+import dynamic from 'next/dynamic';
+
+const NextStudioLoaded = dynamic(
+  () => import('./StudioInner').then((m) => m.default),
+  { ssr: false, loading: () => <div style={{ padding: 40 }}>Loading Studio…</div> }
+);
 
 export default function StudioClient() {
-  return <NextStudio config={config} />;
+  return <NextStudioLoaded />;
 }
