c1e14e43dd51598febe6ea7b09af9934747f23ee
Tj Cichecki <tj@wrkhrs.co>
2026-05-01 09:33:33 -0400

    Auto-commit changes

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import siteSettings from './siteSettings';
+import contactInfo from './contactInfo';
+import navigation from './navigation';
+import page from './page';
+import service from './service';
+import closingStep from './closingStep';
+import value from './value';
+import utilityListing from './utilityListing';
+
+export const schemaTypes = [
+  siteSettings,
+  contactInfo,
+  navigation,
+  page,
+  service,
+  closingStep,
+  value,
+  utilityListing,
+];
