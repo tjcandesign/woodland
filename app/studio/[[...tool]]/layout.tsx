2bf13d73a0ac60351d946a122931ee1d8cea6c43
Tj Cichecki <tj@wrkhrs.co>
2026-05-01 09:34:01 -0400

    Auto-commit changes

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Woodland Studio',
+  robots: 'noindex',
+};
+
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
