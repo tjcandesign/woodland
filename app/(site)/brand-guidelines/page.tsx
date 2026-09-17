044021f541933c553d5a32b8c95900370dfaf1f4
Tj Cichecki <tj@wrkhrs.co>
2026-09-17 14:33:24 -0400

    Auto-commit changes

--- app/(site)/brand-guidelines/page.tsx
+++ app/(site)/brand-guidelines/page.tsx
@@ -11,7 +11,7 @@ export default function BrandGuidelinesRedirect() {
   return (
     <script
       dangerouslySetInnerHTML={{
-        __html: "window.location.replace('/brand.html');",
+        __html: "window.location.replace('/brand/');",
       }}
     />
   );
